7f19ad91ab26fda903a0eab38d69e862bd3f9bb5
1shakti <shakti26.sj@gmail.com>
2024-07-02 11:18:54 +0530

    updated sw file

--- public/sw.js
+++ public/sw.js
@@ -7,23 +7,25 @@ const urlsToCache = [
   '/assets/logos/company_logo.png',
 ];
 
-this.addEventListener('install', event => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-this.addEventListener("fetch", (event) => {
-    if (!navigator.onLine) {
-        event.respondWith(
-            caches.match(event.request).then((resp) => {
-                if (resp) {
-                    return resp
-                }
-                let requestUrl = event.request.clone();
-                fetch(requestUrl)
-            })
-        )
-    }
-}) 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.match(event.request)
+          .then(response => {
+            return response || fetch(event.request)
+              .then(fetchResponse => {
+                cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+              });
+          });
+      })
+  );
+});
